573196cefc47f521574f83ace3dd4472f9d76e36
Stellarix Miu <krisnavincentius407@gmail.com>
2022-07-21 01:31:54 +0700

    miu commit

--- src/controller/verifyToken.js
+++ src/controller/verifyToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import { createError } from '../controller/errorHandling.js'
 
 export const verifyToken = (req, res, next) => {
-  const authHeader = req.headers['authorization']
+  const authHeader = req.headers['Authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
   if(!token) return next(createError(401, "You are not authenticated!"))
